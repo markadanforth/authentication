cb22a15eb518481c07c7b069f2d2dba63395553c
markadanforth <49987402+markadanforth@users.noreply.github.com>
2019-04-30 08:30:19 -0500

    formatting
    __init__ instantiated wrong classes

--- private/profile.py
+++ private/profile.py
@@ -2,12 +2,15 @@ from private.http import Http
 from private.cookie import Cookie
 
 class Profile:
-    Cookie = Cookie()
+
+    def __init__(self):
+        self.Cookie = Cookie()
+        self.Http = Http()
 
     def IsAuthorized(self):
         if not self.Cookie.GetCookie("Auth"):
             print("not logged in")
-            Http().Redirect(303, '/signin')
+            self.Http.Redirect(303, '/signin')
             return False
         else:
             return True
